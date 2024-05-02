5a431bb5d1e636c5983007a603e495e61b16d57d
RyuseiSanada <sanadaryuusei@gmail.com>
2024-05-02 16:39:48 +0900

    演習、いちごの詳細

---
+++ example/src/main/java/model/Fruit.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Fruit {
+	private String name;
+	private int price;
+	public Fruit() {}
+	public Fruit(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	public String getName() { return name; }
+	public int getPrice() { return price; }
+}
